e208699b1c55ffc71afea980e238b4ddbbc945a6
Ramon Pacheco <rmpacheco2@gmail.com>
2023-07-16 00:38:03 -0300

    rename file

---
+++ src/app/dataprovider/dataprovider.go
@@ -0,0 +1,46 @@
+package dataprovider
+
+import (
+	"fmt"
+
+	"github.com/ramonmpacheco/poc-go-gorm/app/dataprovider/entity"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type IDatabase interface {
+	Create(value interface{}) *gorm.DB
+}
+
+type Database struct {
+	DB *gorm.DB
+}
+
+func (dbi *Database) Create(value interface{}) *gorm.DB {
+	return dbi.DB.Create(value)
+}
+
+func NewPostgres() *Database {
+	fmt.Println("About to connect to database")
+	dsn := "host=poc-go-gorm-postgres user=postgres password=postgres dbname=poc_db port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	db.AutoMigrate(&entity.Pastel{}, &entity.Ingredient{})
+	return &Database{
+		DB: db,
+	}
+}
+
+func NewSqlite() *Database {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	db.AutoMigrate(&entity.Pastel{}, &entity.Ingredient{})
+	return &Database{
+		DB: db,
+	}
+}
